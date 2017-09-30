5303f9e7ae1092b07bd59b4d4d57bdda34606ea0
Fabien Blume <bluplayz98@gmail.com>
2017-09-30 21:40:56 +0200

    fix LocaleAPI

--- src/main/java/de/bluplayz/locale/LocaleAPI.java
+++ src/main/java/de/bluplayz/locale/LocaleAPI.java
@@ -3,21 +3,23 @@ package de.bluplayz.locale;
 import de.bluplayz.LocaleManager;
 import lombok.Getter;
 
-public abstract class LocaleAPI {
+public class LocaleAPI {
     @Getter
-    private LocaleManager localeManager;
+    private static LocaleManager localeManager;
 
-    public LocaleAPI(LocaleManager localeManager) {
-        this.localeManager = localeManager;
+    public static void init(LocaleManager localeManager) {
+        LocaleAPI.localeManager = localeManager;
     }
 
-    public String translate(String languageCode, String key, Object... args) {
+    public static String translate(String languageCode, String key, Object... args) {
         return translate(getLocaleManager().getLocale(languageCode), key, args);
     }
 
-    public String translate(String key, Object... args) {
+    public static String translate(String key, Object... args) {
         return translate(getLocaleManager().getDefaultLocale(), key, args);
     }
 
-    public abstract String translate(Locale locale, String key, Object... args);
+    public static String translate(Locale locale, String key, Object... args) {
+        return getLocaleManager().getTranslatedMessage(locale, key, args);
+    }
 }
